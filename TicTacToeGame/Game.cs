5dd5f033fec8eae97857f372e60c6f7c7b70eacf
farhinsheikh <farhinsheikh2015@gmail.com>
2021-04-15 23:05:45 +0530

    [Farhin]ADDED:tic tac toe game

---
+++ TicTacToeGame/Game.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class Game
+    {
+        //Initialize the board.
+        public void Initialize(char[,] board)
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = (char)(count++ + '0');
+                }
+            }
+        }
+
+        
+        /// Displays the specified board.
+        public void Display(char[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(" " + board[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        
+        /// Replaces the specified element of board.
+        public bool Replace(char[,] board, char choice, char player1)
+        {
+            bool valid = false;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == choice)
+                    {
+                        board[i, j] = player1;
+                        valid = true;
+                        return valid;
+                    }
+                }
+            }
+
+            return valid;
+        }
+
+       
+        /// checks the win.
+         public bool WinCheck(char[,] board)
+        {
+            Game game = new Game();
+            return game.CheckRow(board) || game.CheckColumn(board) || game.CheckDiagonal(board);
+        }
+
+       
+        /// Checks the row.
+        public bool CheckRow(char[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        
+        /// Checks the column.
+        public bool CheckColumn(char[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+   
+        /// Checks the diagonal.
+        public bool CheckDiagonal(char[,] board)
+        {
+            if ((board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) || (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0]))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+    
+
---
+++ TicTacToeGame/TicTacToe.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class TicTacToe
+    {
+        
+            public static void PlayGame()
+            {
+                //// initialize the variable
+                int count = 0, intChoice;
+               char choice, player1, player2;
+                bool isWin = false;
+                //// create 3*3 char multidimension array
+                char[,] board = new char[3, 3];
+                //// create random object 
+                Random random = new Random();
+                //// create game object
+                Game game = new Game();
+
+             Console.WriteLine("Player 1 is You!");
+             Console.WriteLine("Player 2 is Computer");
+
+            {
+                    //// take input choice in char type
+                    Console.WriteLine( " enter your choice 'x' or '0':");
+                    player1 = Convert.ToChar(Console.ReadLine());
+                }
+
+                {
+                    Console.WriteLine("Invalid Choice");
+                }
+                //// assign player's symbol to player
+                if (player1 == 'X' || player1 == 'x')
+                {
+                    player2 = '0';
+                }
+                else
+                {
+                    player2 = 'X';
+                }
+                //// declare the array 
+                game.Initialize(board);
+                game.Display(board);
+                //// do while loop for executing 1st iteration without any condition
+                do
+                 { 
+                    {
+                        Console.WriteLine("Now, select your choice:");
+                        intChoice = Convert.ToChar(Console.ReadLine());
+                    }
+
+                    {
+                        Console.WriteLine("Invalid Choice");
+                    }
+
+                    if (intChoice >= 48 && intChoice <= 56)
+                    {
+                        choice = Convert.ToChar(intChoice);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Choice");
+                    }
+
+                    bool valid = game.Replace(board, choice, player1);
+                    if (!valid)
+                    {
+                        Console.WriteLine("Invalid Choice");
+                    }
+
+                    game.Display(board);
+
+                    isWin = game.WinCheck(board);
+                    if (isWin)
+                    {
+                        Console.Write("you won the game ");
+                        break;
+                    }
+
+                    count++;
+                    if (count == 5)
+                    {
+                        Console.WriteLine("****Draw****");
+                        break;
+                    }
+
+                    Console.WriteLine("Computer's turn:");
+                    int computerChoice = (int)random.Next(9);
+                    Console.WriteLine(computerChoice);
+                    char computerChoose = (char)(computerChoice + '0');
+                    bool computerValid = game.Replace(board, computerChoose, player2);
+                    if (!computerValid)
+                    {
+                        Console.WriteLine("Invalid Choice");
+ 
+                    }
+
+                    game.Display(board);
+                    isWin = game.WinCheck(board);
+                    if (isWin)
+                    {
+                        Console.Write("Computer won the game");
+                        break;
+                    }
+                }
+                while (isWin == false || count < 5);
+                Console.ReadKey();
+            }
+        }
+    }
+
